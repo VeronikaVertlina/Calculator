266316fbd5f1424698a68a19003480e634604774
ronya78@gmail.com <ronya78@gmail.com>
2022-01-20 01:51:14 +0200

    Add code

--- calculator.js
+++ calculator.js
@@ -1,7 +1,9 @@
 //jshint esversion:6
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
   //console.log(__dirname + "/index.html");
@@ -9,7 +11,12 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-  res.send("Thanks for posting that!");
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num1 + num2;
+
+  res.send("The result of the calculation is " + result);
 });
 
 
