ac936fdb9dd159d954e0606fe95906a21b41b85d
ronya78@gmail.com <ronya78@gmail.com>
2022-01-20 13:01:36 +0200

    Add new page

--- calculator.js
+++ calculator.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
+//---------------Simple Calculator------------------
 app.get("/", function(req, res){
   //console.log(__dirname + "/index.html");
   res.sendFile(__dirname + "/index.html");
@@ -13,10 +14,21 @@ app.get("/", function(req, res){
 app.post("/", function(req, res){
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
-
   var result = num1 + num2;
-
   res.send("The result of the calculation is " + result);
+});
+
+//--------------BMI Calculator---------------------
+app.get("/bmiCalculator", function(req,res){
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function(req,res){
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var bmi = weight / (height * height);
+
+  res.send("Your BMI is " + bmi);
 });
 
 
